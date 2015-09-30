f7030eb36e3996f2f0c96ee68c0650e35f1374cc
lurong <418083501@qq.com>
2015-09-30 12:28:16 +0800

    no message

--- alwaysChatServer/src/lulu/model/LoginUser.java
+++ alwaysChatServer/src/lulu/model/LoginUser.java
@@ -2,6 +2,14 @@ package lulu.model;
 
 public class LoginUser extends BaseUser {
 	
-	
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
 	
 }
